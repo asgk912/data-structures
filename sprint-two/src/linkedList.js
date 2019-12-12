46f68c4ace786cb9a8ccadb8a837ad37ce5caf8a
asgk912 <asgk912@gmail.com>
2019-12-12 11:28:43 -0800

    implement .addToTail method

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,32 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // find tail (make sure handling empty chain)
+    // var activeNode = list.tail;
+    //make a new node with value
+    var newNode = Node(value);
+    //link the node to active node
+    // activeNode.next = newNode;    
+    //assign the node to the tail
+    // list.tail = newNode;
+    list.tail.next = newNode;
+    // if tail is not node, this wil fail
   };
 
   list.removeHead = function() {
+    // find head (make sure handling empty chain)
+    // assign head value to variable
+    // delete head
+    //assign list with rest
+    //assign new value to the head
+    // return old head value
   };
 
   list.contains = function(target) {
+    // step through chain 
+    // if found value, return true
+    // otherwise return false
+
   };
 
   return list;
